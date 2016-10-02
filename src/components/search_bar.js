f7d9cbc96722b1b397431571214fbe2bff691ef2
Arpit Agarwal <agarwalarpit22@gmail.com>
2016-10-02 12:36:24 +0530

    Added state and class based component for searcch bar

--- src/components/search_bar.js
+++ src/components/search_bar.js
@@ -1,7 +1,48 @@
-import React from 'react';
+import React, { Component } from 'react';
 
-const SearchBar = () => {
-    return <input />
-};
+
+//**********//
+
+//Function based component
+// const SearchBar = () => {
+//     return <input />
+// };
+
+//**********//
+
+//Class based Components are used so that components can be aware of what has happened to them since the time they have been rendered.
+// It is a ES6 class based Javascript implementation with proper properties and method.
+
+// These curly braces mean that pull off a property called Component from react and call it Component
+// It is similar to like const Component = React.Component
+// If we don't use that curly braces syntax in import for Component then while defining class we have to use 'extends React.Component'
+
+//!IMPORTANT TIP: Whenever in doubt to understand state add 'Value Of the Input = {this.state.term} on line 34'
+//!IMPORTANT NOTE: Whenever referencing javascript variable inside JSX, always wrap it inside curly braces like on line 34
+
+class SearchBar extends Component {
+    constructor(props){
+        super(props);
+
+        this.state = { term: ''};
+    }
+
+    render () {
+        return (
+            <div>
+                <input onChange={event => this.setState({ term: event.target.value })} />;
+            </div>
+            );
+
+    }
+
+    //delete the handler
+    // onInputChange(event){
+    //     console.log(event.target.value);
+    //
+    // }
+
+}
+
 
 export default SearchBar;
