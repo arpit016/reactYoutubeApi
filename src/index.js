9570021b7ef79924f3cd114e36c1b58b6ffcf97b
Arpit Agarwal <agarwalarpit22@gmail.com>
2016-10-02 10:04:50 +0530

    Added our first component to main App

--- src/index.js
+++ src/index.js
@@ -1,8 +1,19 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import API_KEY from './components/keys';
+import SearchBar from './components/search_bar';
+
+//const API_KEY = API_KEY;
+console.log(API_KEY);
 // Create a new Component and this component should produce some html
 
-const App = () => <div>Hi!</div>;
+
+const App = () => {
+    return ( <div>
+        <SearchBar />
+    </div>
+    );
+}
 
 // Take this generated HTML and put it on the page.(in the DOM)
 ReactDOM.render(<App />, document.querySelector('.container'));
