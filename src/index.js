c56eecc8753c81c654054b02d6c4536318b00249
Arpit Agarwal <agarwalarpit22@gmail.com>
2016-10-04 13:00:07 +0530

    Converted functional App component to class based component and added youtube video search function

--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
-import React from 'react';
+import React, { Component } from 'react';
 import ReactDOM from 'react-dom';
+import YTSearch from 'youtube-api-search';
 import API_KEY from './components/keys';
 import SearchBar from './components/search_bar';
 
@@ -8,11 +9,28 @@ console.log(API_KEY);
 // Create a new Component and this component should produce some html
 
 
-const App = () => {
-    return ( <div>
-        <SearchBar />
-    </div>
-    );
+
+
+
+class App extends Component {
+    constructor(props) {
+        super(props);
+        this.state = { videos: [] };
+
+        YTSearch({key: API_KEY, term: 'surfboard'}, (videos) => {
+            this.setState({ videos }); //ES6 syntax where key and value pair has same name. In our case its videos
+            //this.setState({ videos: videos });
+        });
+    }
+
+    render() {
+
+        return (
+        <div>
+            <SearchBar />
+        </div>
+        );
+    };
 }
 
 // Take this generated HTML and put it on the page.(in the DOM)
